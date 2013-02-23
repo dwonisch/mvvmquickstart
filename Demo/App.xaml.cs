029710a9dbf000b661aa31a25f86a1e356582a68
Daniel Wonisch <daniel.wonisch@gmail.com>
2013-02-23 18:42:26 +0100

    Implemented DataGrid to View available Customers

--- Demo/App.xaml.cs
+++ Demo/App.xaml.cs
@@ -10,6 +10,6 @@ namespace Demo {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application {
+    public partial class App : System.Windows.Application {
     }
 }
