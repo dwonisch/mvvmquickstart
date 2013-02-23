c40f785ef1672ed26f934607c270cbfda226b954
Daniel Wonisch <daniel.wonisch@gmail.com>
2013-02-23 18:32:38 +0100

    Added a demo CustomerRepository implementation

--- Demo.Data/Customer.cs
+++ Demo.Data/Customer.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace Demo.Data {
     public class Customer {
+        public Customer(string name, string address) {
+            Name = name;
+            Address = address;
+        }
+
         public string Name { get; set; }
         public string Address { get; set; }
     }
---
+++ Demo.Data/DemoCustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Data {
+    public class DemoCustomerRepository : ICustomerRepository {
+        public IEnumerable<Customer> SelectAllCustomers() {
+            yield return new Customer("Daniel", "@home");
+            yield return new Customer("Desiree", "@work");
+            yield return new Customer("Max", "@way");
+        }
+    }
+}
