5417cf6ca73c3f93b53b7df7b2ee1510b7dea83e
Daniel Wonisch <daniel.wonisch@gmail.com>
2013-02-23 18:34:47 +0100

    Implemented MainViewModel

---
+++ Demo.Application/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Demo.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Application {
+    public class MainViewModel {
+        public MainViewModel(ICustomerRepository repository) {
+            Customers = new ObservableCollection<Customer>(repository.SelectAllCustomers());
+        }
+
+        public ICollection<Customer> Customers { get; private set; }
+    }
+}
